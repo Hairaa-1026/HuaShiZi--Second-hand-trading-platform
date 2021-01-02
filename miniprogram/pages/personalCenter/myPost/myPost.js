1ef9dfbc5dd73f0e5eb35b0d55c67f988a7aff2d
1216873721@qq.com <1216873721@qq.com>
2021-01-02 17:22:45 +0800

    WF show

--- miniprogram/pages/personalCenter/myPost/myPost.js
+++ miniprogram/pages/personalCenter/myPost/myPost.js
@@ -1,6 +1,8 @@
-// pages/personalCenter/myPost/myPost.js
-Page({
+// pages/personalCenter/myPost/myPost.
+var Bmob = require('../../../utils/bmob.js')
+var util = require('../../../utils/util.js')
 
+Page({
   /**
    * 页面的初始数据
    */
@@ -35,61 +37,137 @@ Page({
         discription: "超级无敌棒棒棒的书！",
       },
     ],
+    postList:[],
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
+  onLoad: function () {
+    that = this;
+    var Post = Bmob.Object.extend("post");
+    var query = new Bmob.Query(Post);
+    query.equalTo("ownerId", Bmob.User.current().id);
+    query.descending('updatedAt');
+    query.limit(this.data.limit);
+    query.find({
+      success: function (results) {
+        that.setData({
+          postList: results,
+          skip: results.length
+        })
+      },
+      error: function (error) {
+        console.log("onLoad查询post失败: " + error.code + " " + error.message);
+      }
+    })
 
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  onShow:function(){
+    var Post = Bmob.Object.extend("post");
+    var query = new Bmob.Query(Post);
+    query.equalTo("ownerId", Bmob.User.current().id);
+    query.descending('updatedAt');
+    query.limit(this.data.limit);
+    query.find({
+      success: function (results) {
+        that.setData({
+          postList: results,
+          skip: results.length
+        })
+      },
+      error: function (error) {
+        console.log("onShow查询post失败: " + error.code + " " + error.message);
+      }
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
   onPullDownRefresh: function () {
+    wx.setNavigationBarTitle({
+      title: '我的发布'
+    });
+    wx.showNavigationBarLoading(); //在标题栏中显示加载图标
+    setTimeout(function () {
+      wx.stopPullDownRefresh(); //停止加载
+      wx.hideNavigationBarLoading(); //隐藏加载icon
+    }, 2000)
 
   },
 
-  /**
-   * 页面上拉触底事件的处理函数
-   */
   onReachBottom: function () {
-
+    var Post = Bmob.Object.extend("post");
+    var query = new Bmob.Query(Post);
+    query.equalTo("ownerId", Bmob.User.current().id);
+    query.descending('updatedAt');
+    query.skip(this.data.skip);
+    query.limit(this.data.limit);
+    query.find({
+      success: function (results) {
+        if (results.length > 0) {
+          var nl = that.data.postList.concat(results);
+          that.setData({
+            skip: that.data.skip + results.length,
+            postList: nl
+          })
+        }
+        else {
+          wx.showToast({
+            title: '已全部加载',
+            icon: 'success',
+            duration: 3000
+          })
+        }
+      },
+      error: function (error) {
+        console.log("onReachBottom查询post失败: " + error.code + " " + error.message);
+      }
+    })
   },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
+  finishPost: function (event) {
+    var objectId = event.target.dataset.id;
+    wx.showModal({
+      title: '操作提示',
+      content: '请确定当前交易已完成',
+      success: function (res) {
+        if (res.confirm) {
+          //删除日记
+          var Post = Bmob.Object.extend("post");
+          //创建查询对象，入口参数是对象类的实例
+          var query = new Bmob.Query(Post);
+          query.equalTo("objectId", objectId);
+          query.destroyAll({
+            success: function () {
+              common.showTip('删除成功');
+              that.onShow();
+            },
+            error: function (err) {
+              common.showTip('删除失败', 'loading');
+            }
+          });
+        }
+      }
+    })
+  },
+  deletePost: function (event) {
+    var objectId = event.target.dataset.id;
+    wx.showModal({
+      title: '操作提示',
+      content: '确定要删除该发布？',
+      success: function (res) {
+        if (res.confirm) {
+          //删除日记
+          var Post = Bmob.Object.extend("post");
+          //创建查询对象，入口参数是对象类的实例
+          var query = new Bmob.Query(Post);
+          query.equalTo("objectId", objectId);
+          query.destroyAll({
+            success: function () {
+              common.showTip('删除成功');
+              that.onShow();
+            },
+            error: function (err) {
+              common.showTip('删除失败', 'loading');
+            }
+          });
+        }
+      }
+    })
+  },
 })
