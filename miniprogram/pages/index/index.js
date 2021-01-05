6cc7f9beb7c8de42350e1bbf3728e3374db70f7c
yqwang306 <1152297713@qq.com>
2021-01-06 04:23:47 +0800

    update by wyq

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -4,37 +4,9 @@ const app = getApp()
 
 Page({
   data: {
-    keyword:'',
-    navbarActiveIndex: 0,
-    navbarTitle: [
-    "首页",
-    "服装鞋帽",
-    "教材书籍",
-    "美妆护肤",
-    "卡券交易"
-    ]
+    goodsList:[]
   },
   
-  onNavBarTap: function (event) {
-      // 获取点击的navbar的index
-      let navbarTapIndex = event.currentTarget.dataset.navbarIndex
-      // 设置data属性中的navbarActiveIndex为当前点击的navbar
-      this.setData({
-          navbarActiveIndex: navbarTapIndex      
-      })
-   },
-  
-   onBindAnimationFinish: function ({detail}) {
-      // 设置data属性中的navbarActiveIndex为当前点击的navbar
-      this.setData({
-          navbarActiveIndex: detail.current
-      })
-   },
-
-   canvasIdErrorCallback: function (e) {
-    console.error(e.detail.errMsg)
-   },
-
    //获取输入框内容
   searchGood: function(e){
     this.setData({
@@ -54,59 +26,21 @@ Page({
       url: '/pages/release/release'
     })
   },
-  searchBar(e) {
-    var that = this;
-    var keyWord = that.data.keyWord;
-    var value = e.detail.value; //input输入的值
-    console.log(value);
-    that.setData({
-      keyWord: value
-    })
-    // var url = app.globalData.doubanBase + app.globalData.ResourcesURL + value + "&&start=0&&count=10"; 
 
-    var url = app.globalData.huanbaoBase + 'search.php';
-    //微信请求方式的写法
-  
+  onLoad: function(options) {
+    var that = this;
     wx.request({
-      url,
+      url:'http://localhost/viewHomepage.php',
       method: 'POST',
-      data: {
-        keyword: value
-      },
       header: { 'content-type': 'application/x-www-form-urlencoded ' },
       success(res) {
-        console.log(res);
-        that.handleData(res.data.data);
+        that.setData({
+          goodsList: res.data.data
+        })
       },
       fail(err) {
         console.log(err);
       }
     })
-  },
-  handleData(data) {
-    var resultList = []
-    data.forEach(item => {
-      //注意： push可以是具体的对象
-      // resultList.push({
-      //   title: item.title,
-      //   image: item.images.small,
-      //   desc,
-      //   id: item.id
-      // })
-      resultList.push(item)
-    })
-    this.setData({ resultList: resultList });//将此处resultList的值放在数据data的resultList中
-
-  },
-  bindToSearchList(e) {
-    console.log(e.currentTarget.dataset.value);
-    var that = this;
-    var url = app.globalData.huanbaoBase + 'search.php';
-    var value = e.currentTarget.dataset.value;
-    var keyWord = value;
-    wx.navigateTo({
-      url: '../release/release?keyWord=' + keyWord
-    })
   }
-
 })
