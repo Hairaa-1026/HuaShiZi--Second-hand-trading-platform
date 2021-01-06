af7b3d5626850b3f2c50d9d4eb5bb16ef3098fb4
Hairaa-1026 <1020071381@qq.com>
2021-01-06 10:08:26 +0800

    update apis

--- miniprogram/api/searchByCategory.php
+++ miniprogram/api/searchByCategory.php
@@ -19,12 +19,16 @@ function searchByCategory(){
         die(json_encode($data) . mysqli_error($conn));//.mysqli_error($conn));
     }
     while($row = mysqli_fetch_array($result)){
+        $sql = "SELECT userName from user WHERE id='{$row['creatorId']}'";
+        $result2 = mysqli_query($conn,$sql);
+        $row2 = mysqli_fetch_array($result2);
         array_push($data,array(
             'productId'=>$row['id'],
             'thumbnail'=>$row['thumbnail'],
             'title'=>$row['title'],
             'campus'=>$row['campus'],
             'pickupWay'=>$row['pickupWay'],
+            'userName'=>$row2[0],
             'creatorId'=>$row['creatorId'],
             'type'=>$row['type'],
             'price'=>$row['price']
--- miniprogram/api/searchByKeyWord.php
+++ miniprogram/api/searchByKeyWord.php
@@ -19,13 +19,16 @@ function searchByKeyWord(){
         die(json_encode($data) . mysqli_error($conn));//.mysqli_error($conn));
     }
     while($row = mysqli_fetch_array($result)){
+        $sql = "SELECT userName from user WHERE id='{$row['creatorId']}'";
+        $result2 = mysqli_query($conn,$sql);
+        $row2 = mysqli_fetch_array($result2);
         array_push($data,array(
             'productId'=>$row['id'],
             'thumbnail'=>$row['thumbnail'],
             'title'=>$row['title'],
             'campus'=>$row['campus'],
             'pickupWay'=>$row['pickupWay'],
-            'creatorId'=>$row['creatorId'],
+            'userName'=>$row2[0],
             'type'=>$row['type'],
             'price'=>$row['price']
         ));
